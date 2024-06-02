5e51426a944024640d8c152f360eabe2b60decfc
abdullahmoosani <abdullahmoosani12@gmail.com>
2024-06-02 22:25:42 +0500

    To Do List

---
+++ index.js
@@ -0,0 +1,32 @@
+import inquirer from "inquirer";
+let todos = [];
+let condition = true;
+while (condition) {
+    let addTasks = await inquirer.prompt([
+        {
+            name: "todo",
+            type: "input",
+            message: "what do you want add in yours Todos?"
+        },
+        {
+            name: "addMore",
+            type: "confirm",
+            message: "are you sure you want to add in your todos",
+            default: "false"
+        },
+        {
+            name: "removeAny",
+            type: "input",
+            message: "Please provide the index of item to remove",
+            // default : "false"
+        },
+    ]);
+    todos.push(addTasks.todo);
+    todos.pop();
+    condition = addTasks.addMore;
+    console.log(todos);
+    // if(todos == true){
+    // todos.splice(todos, 1)
+    // }
+}
+;
